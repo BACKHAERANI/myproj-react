9a44a6d6e67a9fc6a1169fad6a5c2c198a1ee4ba
back hae ran <idg9833@gmail.com>
2022-01-07 13:24:19 +0900

    Navigate 적용

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import Profile from 'pages/accounts/Profile';
 import Login from 'pages/accounts/Login';
 import ReviewList from 'pages/reviews/ReviewList';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import TopNav from 'components/TopNav';
 import 'App.css';
 
 function App() {
   return (
     <div className="app">
+      <TopNav />
       <Routes>
-        <Route path="/" element={<TopNav />} />
+        <Route path="/" element={<Navigate to="/reviews/" />} />
         <Route path="/accounts/login" element={<Login />} />
         <Route path="/accounts/profile" element={<Profile />} />
         <Route path="/reviews/" element={<ReviewList />} />
