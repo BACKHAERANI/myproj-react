7db76a1e15a2d34f20a52581d0cf6e2633233333
back hae ran <idg9833@gmail.com>
2022-01-07 13:20:35 +0900

    Routes,Route 적용/ TopNav Link 적용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import Profile from 'pages/accounts/Profile';
+import Login from 'pages/accounts/Login';
+import ReviewList from 'pages/reviews/ReviewList';
+import { Route, Routes } from 'react-router-dom';
+import TopNav from 'components/TopNav';
+import 'App.css';
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<TopNav />} />
+        <Route path="/accounts/login" element={<Login />} />
+        <Route path="/accounts/profile" element={<Profile />} />
+        <Route path="/reviews/" element={<ReviewList />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
