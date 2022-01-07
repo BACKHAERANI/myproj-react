f789eafcc3a56b19f63eb841db42977800d60fc4
back hae ran <idg9833@gmail.com>
2022-01-07 17:13:10 +0900

    ReviewForm 폼 내용 구현

--- src/pages/reviews/ReviewForm.jsx
+++ src/pages/reviews/ReviewForm.jsx
@@ -2,6 +2,19 @@ function ReviewForm() {
   return (
     <div>
       <h2>Review Form</h2>
+      <h3>평점</h3>
+      <select name="score">
+        <option>0</option>
+        <option>1</option>
+        <option>2</option>
+        <option>3</option>
+        <option>4</option>
+        <option>5</option>
+      </select>
+      <h3>리뷰</h3>
+      <textarea type="text" name="content"></textarea>
+      <hr />
+      <button onClick={() => {}}>저장하기</button>
     </div>
   );
 }
