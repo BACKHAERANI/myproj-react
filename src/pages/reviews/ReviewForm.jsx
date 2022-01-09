ba6e840f63fd91984e517799f51e2db82da9eee4
back hae ran <idg9833@gmail.com>
2022-01-09 20:21:58 +0900

    새로운 리뷰 등록 폼 구현

--- src/pages/reviews/ReviewForm.jsx
+++ src/pages/reviews/ReviewForm.jsx
@@ -1,9 +1,29 @@
+import Axios from 'axios';
+import useFieldValues from 'components/hooks/useFieldValues';
+import { useNavigate } from 'react-router-dom';
+
 function ReviewForm() {
+  const navigate = useNavigate();
+  const [fieldValues, handleChange, setFieldValues] = useFieldValues({
+    content: '',
+    score: 5,
+  });
+
+  const createReview = () => {
+    const url = 'http://127.0.0.1:8000/shop/api/reviews/';
+    Axios.post(url, fieldValues)
+      .then(() => navigate('/reviews/'))
+      .catch((error) => {
+        console.error(error);
+      })
+      .finally(() => setFieldValues({}));
+  };
+
   return (
     <div>
       <h2>Review Form</h2>
       <h3>평점</h3>
-      <select name="score">
+      <select name="score" onChange={handleChange}>
         <option>0</option>
         <option>1</option>
         <option>2</option>
@@ -12,9 +32,16 @@ function ReviewForm() {
         <option>5</option>
       </select>
       <h3>리뷰</h3>
-      <textarea type="text" name="content"></textarea>
+      <textarea type="text" name="content" onChange={handleChange}></textarea>
       <hr />
-      <button onClick={() => {}}>저장하기</button>
+      <button
+        onClick={() => {
+          navigate('/reviews/');
+          createReview();
+        }}
+      >
+        저장
+      </button>
     </div>
   );
 }
