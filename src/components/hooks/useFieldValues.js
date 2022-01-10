bf536147120a7b8fd8452e532e6448bc238f5707
back hae ran <idg9833@gmail.com>
2022-01-10 13:23:16 +0900

    edit 구현 /useCallback 사용하여 구현

--- src/components/hooks/useFieldValues.js
+++ src/components/hooks/useFieldValues.js
@@ -1,19 +1,21 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 function useFieldValues(initialFieldValues) {
   const [fieldValues, setFieldValues] = useState(initialFieldValues);
 
-  const handleChange = (e) => {
+  // 함수 객체를 생성할 때, 의존성이 걸린 값이 변경시에만 함수를 재생성
+
+  const handleChange = useCallback((e) => {
     const { name, value } = e.target;
     setFieldValues((prevFieldValues) => ({
       ...prevFieldValues,
       [name]: value,
     }));
-  };
+  }, []);
 
-  const clearFieldValues = () => {
+  const clearFieldValues = useCallback(() => {
     setFieldValues(initialFieldValues);
-  };
+  }, []);
 
   return { fieldValues, handleChange, clearFieldValues, setFieldValues };
 }
