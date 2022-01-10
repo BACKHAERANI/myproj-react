11a7dd563eb10bc99c4d3bd512df57884bff2bff
back hae ran <idg9833@gmail.com>
2022-01-10 16:34:07 +0900

    Merge branch로 변경된  ReviewForm/useFieldValues 되돌리기

--- src/components/hooks/useFieldValues.js
+++ src/components/hooks/useFieldValues.js
@@ -1,25 +1,16 @@
-<<<<<<< HEAD
 import { useCallback, useState } from 'react';
-=======
-import { useState } from 'react';
->>>>>>> homework/39
 
 function useFieldValues(initialFieldValues) {
   const [fieldValues, setFieldValues] = useState(initialFieldValues);
 
-<<<<<<< HEAD
   // 함수 객체를 생성할 때, 의존성이 걸린 값이 변경시에만 함수를 재생성
 
   const handleChange = useCallback((e) => {
-=======
-  const handleChange = (e) => {
->>>>>>> homework/39
     const { name, value } = e.target;
     setFieldValues((prevFieldValues) => ({
       ...prevFieldValues,
       [name]: value,
     }));
-<<<<<<< HEAD
   }, []);
 
   const clearFieldValues = useCallback(() => {
@@ -27,11 +18,5 @@ function useFieldValues(initialFieldValues) {
   }, []);
 
   return { fieldValues, handleChange, clearFieldValues, setFieldValues };
-=======
-  };
-
-  return [fieldValues, handleChange, setFieldValues];
->>>>>>> homework/39
 }
-
 export default useFieldValues;
--- src/pages/reviews/ReviewForm.jsx
+++ src/pages/reviews/ReviewForm.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import useFieldValues from 'components/hooks/useFieldValues';
 import { useNavigate, useParams } from 'react-router-dom';
 import Axios from 'axios';
@@ -6,7 +5,6 @@ import DebugStates from 'components/DebugStates';
 import ReviewForm from 'components/ReviewForm';
 import { useState } from 'react';
 import { useEffect } from 'react';
-
 function PageReviewForm() {
   const { reviewId } = useParams(); // 파람스 ':'으로 시작하는 값들을 가져온다
   const [loading, setLoading] = useState(false);
@@ -17,12 +15,10 @@ function PageReviewForm() {
       content: '',
       score: 5,
     });
-
   useEffect(() => {
     const fetchReview = async () => {
       setLoading(true);
       setError(null);
-
       const url = `http://localhost:8000/shop/api/reviews/${reviewId}/`;
       try {
         const response = await Axios.get(url);
@@ -35,7 +31,6 @@ function PageReviewForm() {
     if (reviewId) fetchReview();
     else clearFieldValues();
   }, [reviewId, setFieldValues, clearFieldValues]);
-
   const saveReview = async () => {
     setLoading(true);
     setError(null);
@@ -58,34 +53,10 @@ function PageReviewForm() {
       setError(e);
       console.error(e);
     }
-
     setLoading(false);
-=======
-import Axios from 'axios';
-import useFieldValues from 'components/hooks/useFieldValues';
-import { useNavigate } from 'react-router-dom';
-
-function ReviewForm() {
-  const navigate = useNavigate();
-  const [fieldValues, handleChange, setFieldValues] = useFieldValues({
-    content: '',
-    score: 5,
-  });
-
-  const createReview = () => {
-    const url = 'http://127.0.0.1:8000/shop/api/reviews/';
-    Axios.post(url, fieldValues)
-      .then(() => navigate('/reviews/'))
-      .catch((error) => {
-        console.error(error);
-      })
-      .finally(() => setFieldValues({}));
->>>>>>> homework/39
   };
-
   return (
     <div>
-<<<<<<< HEAD
       <h2>Review Form {reviewId ? '수정' : '생성'}</h2>
       <DebugStates reviewId={reviewId} />
       <ReviewForm
@@ -94,29 +65,6 @@ function ReviewForm() {
         handleSubmit={() => saveReview()}
         loading={loading}
       />
-=======
-      <h2>Review Form</h2>
-      <h3>평점</h3>
-      <select name="score" onChange={handleChange}>
-        <option>0</option>
-        <option>1</option>
-        <option>2</option>
-        <option>3</option>
-        <option>4</option>
-        <option>5</option>
-      </select>
-      <h3>리뷰</h3>
-      <textarea type="text" name="content" onChange={handleChange}></textarea>
-      <hr />
-      <button
-        onClick={() => {
-          navigate('/reviews/');
-          createReview();
-        }}
-      >
-        저장
-      </button>
->>>>>>> homework/39
     </div>
   );
 }
