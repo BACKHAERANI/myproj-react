854450646cfe2048a6c0537f9570526249899035
back hae ran <idg9833@gmail.com>
2022-01-12 16:27:30 +0900

    Delete 구현 //

--- src/news/compoents/ArticleDetail.jsx
+++ src/news/compoents/ArticleDetail.jsx
@@ -1,14 +1,45 @@
 import { useApiAxios } from 'api/base';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import LoadingIndicator from './LoadingIndicator';
 
 function ArticleDetail({ articleId }) {
-  const [{ data: article, loading, error }] = useApiAxios(
-    `/news/api/article/${articleId}`,
+  const navigate = useNavigate();
+
+  const [{ data: article, loading, error }, refetch] = useApiAxios(
+    `/news/api/article/${articleId}/`,
   );
+
+  const [{ loading: deleteLoading, error: deleteError }, deleteArticle] =
+    useApiAxios(
+      {
+        url: `/news/api/article/${articleId}/`,
+        method: 'DELETE',
+      },
+      { manual: true },
+    );
+
+  const handleDelete = () => {
+    if (window.confirm('Are you sure?')) {
+      // REST API 에서는 DELETE 요청에 대한 응답이 없습니다.
+      deleteArticle().then(() => {
+        navigate('/news/');
+      });
+    }
+  };
+
+  useEffect(() => {
+    refetch();
+  }, []);
+
   return (
     <div>
-      {loading && '로딩 중...'}
-      {error && '로딩 중 에러가 발생했습니다.'}
+      {loading && <LoadingIndicator />}
+      {deleteLoading && <LoadingIndicator>삭제 중 ...</LoadingIndicator>}
+      {error &&
+        `로딩 중 에러가 발생했습니다. (${error.response.status} ${error.response.statusText})`}
+      {deleteError &&
+        `삭제 요청 중 에러가 발생했습니다. (${deleteError.response.status} ${deleteError.response.statusText})`}
       {article && (
         <>
           <h3 className="text-2xl my-5">{article.title}</h3>
@@ -22,13 +53,20 @@ function ArticleDetail({ articleId }) {
         </>
       )}
       <hr className="my-3" />
-      <div className="flex gap-4 mb-10">
-        <Link className="hover:text-red-400" to="/news/">
-          목록
+      <div className="flex gap-4 mt-3 mb-10">
+        <Link to="/news/" className="hover:text-red-400">
+          목록으로
         </Link>
-        <Link className="hover:text-blue-400" to={`/news/${articleId}/edit/`}>
-          수정
+        <Link to={`/news/${articleId}/edit/`} className="hover:text-red-400">
+          수정하기
         </Link>
+        <button
+          disabled={deleteLoading}
+          onClick={handleDelete}
+          className="hover:text-red-400"
+        >
+          삭제하기
+        </button>
       </div>
     </div>
   );
