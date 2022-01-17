c1002b780e5371da2d8877b159c5e44bb18c9099
back hae ran <idg9833@gmail.com>
2022-01-17 16:59:26 +0900

    로그인 구현 // username, password를 통한 token 획득 구현 2

--- src/pages/accounts/PageLogin.jsx
+++ src/pages/accounts/PageLogin.jsx
@@ -1,67 +1,70 @@
-import H1 from 'news/compoents/H1';
-import H3 from 'news/compoents/H3';
-import useFieldValues from 'components/hooks/useFieldValues';
-import { useApiAxios } from 'api/base';
 import DebugStates from 'components/DebugStates';
+import { useApiAxios } from 'api/base';
+import useFieldValues from 'components/hooks/useFieldValues';
+import H1 from 'news/compoents/H1';
 
-const INIT_FIELD_VALUES = { username: '', password: '' };
+const INITIAL_FIELD_VALUES = { username: '', password: '' };
 
 function PageLogin() {
-  const [
-    { data: accounts, loading: accountLoading, error: accountError },
-    requestToken,
-  ] = useApiAxios(
-    { url: `/accounts/api/token/`, method: 'POST' },
+  const [{ loading, error }, requestToken] = useApiAxios(
+    {
+      url: '/accounts/api/token/',
+      method: 'POST',
+    },
     { manual: true },
   );
 
-  const { fieldValues, handleFieldChange } = useFieldValues(
-    accounts || INIT_FIELD_VALUES,
-  );
+  const { fieldValues, handleFieldChange } =
+    useFieldValues(INITIAL_FIELD_VALUES);
 
   const handleSubmit = (e) => {
+    e.preventDefault();
+
     requestToken({ data: fieldValues }).then((response) => {
-      console.log(response.data);
+      const { access, refresh } = response.data;
+      console.log('access :', access);
+      console.log('refresh :', refresh);
     });
   };
 
   return (
     <div>
-      {accountLoading && '로딩 중'}
-      {accountError && '에러가 발생했습니다.'}
-
       <H1>Login</H1>
-      <div className="my-5">
-        <H3>Username : </H3>
-        <input
-          type="text"
-          name="username"
-          value={fieldValues.username}
-          onChange={handleFieldChange}
-          className="border outline-none hover:border-orange-500"
-        />
-      </div>
-      <div className="my-5">
-        <H3>Password : </H3>
-        <input
-          type="password"
-          name="password"
-          value={fieldValues.password}
-          onChange={handleFieldChange}
-          className="border outline-none hover:border-orange-500"
-        />
-      </div>
-      <div className="my-5">
-        <button
-          onClick={handleSubmit}
-          className="h-10 w-40 border-2  border-orange-500 hover:bg-orange-500"
-        >
-          Login
-        </button>
-      </div>
-      <DebugStates accounts={fieldValues} error={accountError} />
+
+      {error?.response?.status === 401 && (
+        <div className="text-red-400">로그인에 실패했습니다.</div>
+      )}
+
+      <form onSubmit={handleSubmit}>
+        <div className="my-5">
+          <input
+            type="text"
+            name="username"
+            value={fieldValues.username}
+            onChange={handleFieldChange}
+            placeholder="username"
+            className="p-3 bg-orange-100 focus:outline-none focus:border-2 focus:border-orange-500 w-full"
+          />
+        </div>
+        <div className="my-5">
+          <input
+            type="password"
+            name="password"
+            value={fieldValues.password}
+            onChange={handleFieldChange}
+            placeholder="passowrd"
+            className="p-3 bg-orange-100 focus:outline-none focus:border-2 focus:border-orange-500 w-full"
+          />
+        </div>
+        <div className="my-5">
+          <button className="w-full h-10 border-2 border-orange-500 hover:bg-orange-500">
+            Login
+          </button>
+        </div>
+      </form>
+
+      <DebugStates fieldValues={fieldValues} />
     </div>
   );
 }
-
 export default PageLogin;
