28cd71fb0a4d802a00e72f438ca80923450a521b
back hae ran <idg9833@gmail.com>
2022-01-18 10:20:44 +0900

    Login 구현 // PageLogin->LoginForm으로 이동

--- src/pages/accounts/PageLogin.jsx
+++ src/pages/accounts/PageLogin.jsx
@@ -1,69 +1,9 @@
-import DebugStates from 'components/DebugStates';
-import { useApiAxios } from 'api/base';
-import useFieldValues from 'components/hooks/useFieldValues';
-import H1 from 'news/compoents/H1';
-
-const INITIAL_FIELD_VALUES = { username: '', password: '' };
+import LoginForm from './LoginForm';
 
 function PageLogin() {
-  const [{ loading, error }, requestToken] = useApiAxios(
-    {
-      url: '/accounts/api/token/',
-      method: 'POST',
-    },
-    { manual: true },
-  );
-
-  const { fieldValues, handleFieldChange } =
-    useFieldValues(INITIAL_FIELD_VALUES);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    requestToken({ data: fieldValues }).then((response) => {
-      const { access, refresh } = response.data;
-      console.log('access :', access);
-      console.log('refresh :', refresh);
-    });
-  };
-
   return (
     <div>
-      <H1>Login</H1>
-
-      {error?.response?.status === 401 && (
-        <div className="text-red-400">로그인에 실패했습니다.</div>
-      )}
-
-      <form onSubmit={handleSubmit}>
-        <div className="my-5">
-          <input
-            type="text"
-            name="username"
-            value={fieldValues.username}
-            onChange={handleFieldChange}
-            placeholder="username"
-            className="p-3 bg-orange-100 focus:outline-none focus:border-2 focus:border-orange-500 w-full"
-          />
-        </div>
-        <div className="my-5">
-          <input
-            type="password"
-            name="password"
-            value={fieldValues.password}
-            onChange={handleFieldChange}
-            placeholder="passowrd"
-            className="p-3 bg-orange-100 focus:outline-none focus:border-2 focus:border-orange-500 w-full"
-          />
-        </div>
-        <div className="my-5">
-          <button className="w-full h-10 border-2 border-orange-500 hover:bg-orange-500">
-            Login
-          </button>
-        </div>
-      </form>
-
-      <DebugStates fieldValues={fieldValues} />
+      <LoginForm />
     </div>
   );
 }
